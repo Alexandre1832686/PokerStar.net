27fe45e8d1357c81fe902cf3e20c742f80e91e23
francis2031420 <2031420@etu.cchic.ca>
2022-10-20 13:02:10 -0400

    MainJoueur

--- 2-Conception/PokerStar/PokerStar/MainJoueur.cs
+++ 2-Conception/PokerStar/PokerStar/MainJoueur.cs
@@ -8,5 +8,21 @@ namespace PokerStar
 {
     internal class MainJoueur
     {
+        Carte[] laMain = new Carte[2];
+        int force;
+            
+        public MainJoueur(Carte[] carteDonner)
+        {
+            laMain = carteDonner;
+        }
+        public int Comparer(MainJoueur lautreMAin)
+        {
+            return 0;
+        }
+        public void CalculerForce()
+        {
+               
+        }
     }
+   
 }
