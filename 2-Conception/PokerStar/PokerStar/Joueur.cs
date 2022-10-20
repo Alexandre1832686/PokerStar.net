eaed54b4edb63d7ab5ef6d63c13ce92aeb0b36ae
MathieuDionne2032450 <2032450@etu.cchic.ca>
2022-10-20 13:03:35 -0400

    Mathieu Dionne 2022-10-20

--- 2-Conception/PokerStar/PokerStar/Joueur.cs
+++ 2-Conception/PokerStar/PokerStar/Joueur.cs
@@ -1,12 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PokerStar
 {
+
+
     internal class Joueur
     {
+        //déclaration des variables
+        string nom;
+        string pseudo;
+        int argent;
+        bool actif;
+        //Mainjoueur mainJoueur;
+
+        //Constructeur de Joueur
+        public Joueur(string nom, string pseudo){
+            this.nom = nom;
+            this.pseudo = pseudo;
+            argent = 300;
+            actif = false;
+            }
+
+        public int Miser(int montant)
+        {
+            if (montant <= argent)
+            {
+                return montant;
+            }
+            else
+            {
+                return argent;
+            }
+        }
+
+        public void Check()
+        {
+            actif = true;
+        }
+
+        public void Coucher()
+        {
+            actif= false;
+        }    
+
+        public void ResetMain()
+        {
+            //mainJoueur = new MainJoueur
+        }
     }
 }
