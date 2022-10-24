0fc9ee7fcffbb311229a4fff6dc0b6929dbfa26b
dev-2031420 <2031420@etu.cchic.ca>
2022-10-24 12:24:02 -0400

    tour finaliser 2022-10-24 12;23

--- 2-Conception/PokerStar/PokerStar/Tour.cs
+++ 2-Conception/PokerStar/PokerStar/Tour.cs
@@ -14,13 +14,39 @@ namespace PokerStar
         {
             
         }
+        /// <summary>
+        /// 
+        /// </summary>
         public void ChangerEtat()
         {
-            
+            int position = 0;
+            int inbCarteAtourner = 0;
+            if(etatTour==1)
+            {
+                position = 0;
+                inbCarteAtourner = 3;
+            }
+            else if(etatTour==2 )
+            {
+                position = 3;
+                inbCarteAtourner = 1;
+            }
+            else if(etatTour == 3)
+            {
+                position=4;
+                inbCarteAtourner = 1;
+            }
+            for (int i = position; i < inbCarteAtourner; i++)
+            {
+                carteCommune[i].retourner();
+            }
         }
         public void ResetTour()
         {
             // carteCommune= Paquet.Distribuer(t: Tour)
         }
+        public void AugmenterEtatTour()
+        {etatTour++;}
+
     }
 }
